9873f222b86c35af505e0b5371240125ce9ad779
Sean Dez <sd@seandezoysa.com>
2020-10-15 04:07:25 +0700

    fix form radio button values

--- src/AuthView/Originals/RepeatRuleAdder.tsx
+++ src/AuthView/Originals/RepeatRuleAdder.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import moment from 'moment';
 
 
 export function RepeatRuleAdder({ taskGid }: any) {
   const [addRuleView, setAddRuleView] = React.useState(false);
-  const [timeInterval, setTimeInterval] = React.useState<number>()
+  const [timeInterval, setTimeInterval] = React.useState<number>(2)
   enum TimeUnits { days = 'days', weeks = 'weeks', months = 'months' };
-  const [timeUnit, setTimeUnit] = React.useState<string>(TimeUnits.days);
-  const [startDate, setStartDate] = React.useState<string>('');
+  const [timeUnit, setTimeUnit] = React.useState<string>(TimeUnits.weeks);
+
+  const today: string = moment().format('YYYY-MM-DD');
+  const [startDate, setStartDate] = React.useState<string>(today);
 
 
   if (addRuleView === false) {
@@ -26,29 +29,46 @@ export function RepeatRuleAdder({ taskGid }: any) {
         <div>
           <span>Every </span>
           <span>
-            <input type='number' onBlur={e => setTimeInterval(Number(e.target.value))} />
+            <input 
+              type='number' 
+              onBlur={e => setTimeInterval(Number(e.target.value))} 
+              defaultValue="2"
+            />
           </span>
           <span>
-            <input type='radio' value={TimeUnits.days} onSelect={(e: any) => setTimeUnit(TimeUnits.days)} />
+            <input type='radio' 
+              value={TimeUnits.days} 
+              onChange={(e: any) => setTimeUnit(TimeUnits.days)} 
+            />
             <label>{ TimeUnits.days }</label>
           </span>
           <span>
-            <input type='radio' value={ TimeUnits.weeks } onSelect={(e: any) => setTimeUnit(TimeUnits.weeks)} />
+            <input type='radio' value={ TimeUnits.weeks } onChange={(e: any) => setTimeUnit(TimeUnits.weeks)} checked />
             <label>{ TimeUnits.weeks }</label>
           </span>
           <span>
-            <input type='radio' value={TimeUnits.months} onSelect={(e: any) => setTimeUnit(TimeUnits.months)} />
+            <input type='radio' value={TimeUnits.months} onChange={(e: any) => setTimeUnit(TimeUnits.months)} />
             <label>{ TimeUnits.months }</label>
           </span>
 
           <span>Start Date: </span>
-          <input type="date" onBlur={e => setStartDate(e.target.value)} />
+          <input 
+            type="date" 
+            onBlur={e => setStartDate(e.target.value)} 
+            defaultValue={today}
+          />
         </div>
 
-        <button onClick={() => setAddRuleView(false)}>X Close</button>
-        <button>Create</button>
+        <button onClick={e => { 
+          e.preventDefault();
+          setAddRuleView(false)        
+        }}>X Close</button>
+        <button onClick={e => { 
+          e.preventDefault();        
+        }}>Create</button>
 
-        <button onClick={() => {
+        <button onClick={e => {
+          e.preventDefault();
           console.log('addRuleView', addRuleView);
           console.log('timeInterval', timeInterval);
           console.log('timeUnit', timeUnit);
