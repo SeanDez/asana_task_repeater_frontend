0dba6324b7ccefdf322446a5002ca02732867bdd
Sean Dez <sd@seandezoysa.com>
2020-10-29 21:47:18 +0700

    remove loader div border

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -3,7 +3,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 export default () => (
-  <div style={{ width: '100vw', height: "100vh", border: '2px dashed green' }}>
+  <div style={{ width: '100vw', height: "100vh" }}>
     <CircularProgress
       style={{ margin: '50vh 50vw' }}
     />
