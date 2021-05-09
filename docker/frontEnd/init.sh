bea64a336ca349a506bf0f6c593bec7c25a2f69c
Sean Dezoysa <mrseandezoysa@gmail.com>
2021-05-09 14:15:40 -0400

    add dockerfile and init bash script

---
+++ docker/frontEnd/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# shows exist status of last command
+set -eo pipefail
+
+echo '../init.sh echoed'
+
+# needs https url (no local ssh key in container)
+git clone https://github.com/SeanDez/asana_task_repeater_frontend.git
+cd asana_task_repeater_frontend
+
+echo 'running: npm i'
+npm i
+echo 'install successful ====='
+
+echo 'running: npm run build'
+npm run build
+echo 'build successful ====='
+
+# dev server
+npm start
+
+
